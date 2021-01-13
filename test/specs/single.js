1985ec799ff011fbe97786a4dc994205a03a0032
AnubhavBasu <anubhav.basu10@gmail.com>
2021-01-13 15:46:26 +0530

    commit4

--- test/specs/single.js
+++ test/specs/single.js
@@ -4,45 +4,42 @@ const { expect } = require("chai");
 describe('BrowserStack Local Testing', function() {
   it('can check tunnel working', function () {
     browser.url('http://localhost:8080/')
-    browser.pause(3000)
-    //Taking ScreenShots
-    //browser.saveScreenshot('./screenshots/'+browser.capabilities.browserName+'test.png')
-    console.log('Testing on: ', process.env.ENVIRONMENT)
-    if (process.env.ENVIRONMENT.toString==="BStack") {
-      console.log('Taking PercySnapShot')
-      percySnapshot(browser, browser.capabilities.browserName+'5');
-      console.log('Opening ULR...');
-      browser.getTitle().should.match(/Jenkins/i);
-      console.log('Validating Test');
-      //Tag Tests on BrowserStack
-      session_id = browser.sessionId;
-      console.log('SessionID:', session_id);
+    browser.pause(3000);
+    session_id = browser.sessionId;
+    console.log('SessionID:', session_id);
+    console.log('Testing on: ', process.env.ENVIRONMENT);
+
+    if (process.env.ENVIRONMENT=='BStack') {
+      let timestamp = Date.now().toString;
+      console.log('Taking PercySnapShot at: ',timestamp);
+      percySnapshot(browser, browser.capabilities.browserName+"_"+timestamp);
+      browser.getTitle().should.match(/Jenkins/i)
     } else {
-      console.log('Opening ULR...');
+      console.log('Opening ULR...')
       browser.getTitle().should.match(/Jenkins/i);
       console.log('Validating Test');
-      console.log('Taking Local ScreenShots');
+      console.log('Taking Local SS');
       browser.saveScreenshot('./screenshots/'+browser.capabilities.browserName+'test.png');
     }
-    if(process.env.ENVIRONMENT.toString==="BStack") {
-        console.log('Marking Tests on BS');
+    if(process.env.ENVIRONMENT=='BStack') {
+        console.log('Marking on BStack')
+        var url = "https://"+process.env.BROWSERSTACK_USERNAME+":"+process.env.BROWSERSTACK_ACCESS_KEY+"@api.browserstack.com/automate/sessions/"+session_id+".json";
+        console.log('Marking Tests on BS', url);
         if (browser.getTitle().should.match(/Jenkins/i)) {
-          
-          console.log('Test Passed');
-          request({uri: "https://anubahavbasu1:ysAUqSiaZkL9UqSUrbdL@api.browserstack.com/automate/sessions/"+session_id+".json", method:"PUT", form:{"status":"Passed","reason":""}});
-          browser.pause(2000);
-      
+          console.log('Test Passed')
+          request({uri: url, method:"PUT", form:{"status":"Passed","reason":""}});
+          browser.pause(2000)
       } else {
           console.log('Test Failed');
-          request({uri: "https://anubahavbasu1:ysAUqSiaZkL9UqSUrbdL@api.browserstack.com/automate/sessions/"+session_id+".json", method:"PUT", form:{"status":"Failed","reason":"Title Mismatch"}});
+          request({uri: url, method:"PUT", form:{"status":"Failed","reason":"Title Mismatch"}});
           browser.pause(2000);
 
         }
     }
     else {
-      console.log('Taking Local ScreenShots');
+      console.log('Taking Local ScreenShots', process.env.ENVIRONMENT);
       browser.saveScreenshot('./screenshots/'+browser.capabilities.browserName+'test.png');
     }
-    console.log('Outside if-else')
+    
   });
 });
